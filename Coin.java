3d412d07dc26f23f2f7db73e6ea4b0e94695ce5a
Tim Partridge <partridgete@appstate.edu>
2021-11-03 10:28:09 -0400

    started Coin abstraction

---
+++ Coin.java
@@ -0,0 +1,11 @@
+public abstract class Coin {
+    private String name;
+    private String countryCode;
+    private double denomination;
+
+    public Coin(String n, String c, double d) {
+        name = n;
+        countryCode = c;
+        denomination = d;
+    }
+}
