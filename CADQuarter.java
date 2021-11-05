a9f37456334ceb7b1f5639398b7630d3ac59b660
Tim Partridge <partridgete@appstate.edu>
2021-11-05 10:30:47 -0400

    added constructor for CAD coins

--- CADQuarter.java
+++ CADQuarter.java
@@ -1,3 +1,5 @@
 public class CADQuarter extends Coin {
-
+    public CADQuarter() {
+        super("Quarter", "CAD", 0.25);
+    }
 }
--- Toonie.java
+++ Toonie.java
@@ -1,3 +1,5 @@
 public class Toonie extends Coin {
-
+    public Toonie() {
+        super("Toonie", "CAD", 2.00);
+    }
 }
