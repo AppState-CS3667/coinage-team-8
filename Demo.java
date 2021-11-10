47d51da72347daea228ffb8f23bdd70d4521801f
barbame <barbame@appstate.edu>
2021-11-10 17:15:55 -0500

    PDemo = prototype demo || Demo: workingon making it change factories

--- Demo.java
+++ Demo.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
+
 class Demo { 
     public static void main(String[] args)
     {
-        System.out.println("Making a coin");
-        Coin a = new Quarter();
-        System.out.println("This the name: " + a.getName());
-        System.out.println("This the CountryCode: " + a.getCountryCode());
-        System.out.println("This the Denomination: " + a.getDenomination());  
+        Scanner kb = new Scanner(System.in);
+
+        if (args.length == 0){
+            factory = new USDMint();
+        }
+        else if (args[0].equals("CAD")) {
+            factory = new CADMint();
+        }
+        else {
+            factory = new USDMint();
+        }
+
+
+
+        String denomination = kb.nextLine();
+        
+
+        //System.out.println("Smelting...");
+        //System.out.println("Inspecting...");
+        //System.out.println("Smoothing...");
+        //System.out.println("Buffing...");
+        
+
     }    
 } 
