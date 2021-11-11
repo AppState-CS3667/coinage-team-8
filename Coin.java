32b26e2efcb9cbc4d634fad32611c97c6d388fb1
Tim Partridge <partridgete@appstate.edu>
2021-11-10 23:41:31 -0500

    fixed null coin

--- Coin.java
+++ Coin.java
@@ -45,31 +45,29 @@ public abstract class Coin {
             System.out.println("Inspecting failed");
         }
     }
-    
-    private static class NullCoin extends Coin{
-        @Override
-        public String getName()
-        {
-            return null;
-        }
-
-        @Override
-        public String getCountryCode()
-        {
-            return null;
-        }
-
-        @Override
-        public double getDenomination()
-        {
-            return 0;
-        }
-        public void smelt()
-        {
-
-        }
-
+}
+private class NullCoin extends Coin{
+    @Override
+    public String getName()
+    {
+        return null;
     }
+
+    @Override
+    public String getCountryCode()
+    {
+        return null;
+    }
+
+    @Override
+    public double getDenomination()
+    {
+        return 0;
+    }
+    public void smelt()
+    {
+    }
+}
 
     
     
